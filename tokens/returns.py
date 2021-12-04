a36310b59ff7176086cf7747de67b16034bcaae0
arslan-cyberTITU <rasulov.titu@gmail.com>
2021-12-04 16:33:19 +0500

    commit

--- tokens/returns.py
+++ tokens/returns.py
@@ -1,8 +1,10 @@
 class Returns:
     INSERTED     = {"error" : False, "body" : "INSERTED"}
-    NOT_INSERTED = {"error" : True, "body" : "NOT INSERTED"}
+    NOT_INSERTED = {"error" : True,  "body" : "NOT INSERTED"}
     DELETED      = {"error" : False, "body" : "DELETED"}
-    NOT_DELETED  = {"error" : True, "body" : "NOT DELETED"}
+    NOT_DELETED  = {"error" : True,  "body" : "NOT DELETED"}
+    UPDATED      = {"error" : False, "body" : "UPDATED"}
+    NOT_UPDATED  = {"error" : True,  "body" : "NOT UPDATED"}
     
     def object(obj):
         return {"error" : False, "body" : obj}
