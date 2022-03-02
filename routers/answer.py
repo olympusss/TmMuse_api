f2ebcde7a488c50b01236e232a2fbb1c0bf80935
arslan-cyberTITU <rasulov.titu@gmail.com>
2022-03-02 20:11:42 +0500

    commit

--- routers/answer.py
+++ routers/answer.py
@@ -11,11 +11,14 @@ answers_router = APIRouter()
 @answers_router.get("/answers", dependencies=[Depends(HTTPBearer())])
 def get_answers(header_param: Request, db: Session = Depends(get_db)):
     user_id = crud.read_user_id_from_token(db=db, header_param=header_param)
-    if not user_id:
-        return Returns.USER_NOT_FOUND
-    first_response = crud.read_inbox_by_user_id(db=db, user_id=user_id)
-    second_response = crud.read_inbox(db=db)
-    third_response = crud.read_answered_messages_by_user_id(db=db, user_id=user_id)
+    if user_id:
+        first_response = crud.read_inbox_by_user_id(db=db, user_id=user_id)
+        second_response = crud.read_inbox(db=db)
+        third_response = crud.read_answered_messages_by_user_id(db=db, user_id=user_id)
+    else:
+        first_response = None
+        second_response = crud.read_inbox(db=db)
+        third_response = None
     results = {}
     results["first"] = first_response
     results["second"] = second_response
