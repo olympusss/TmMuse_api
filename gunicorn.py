bdfa9400109eb2ffa5dd0ac7dde4e58f6a950137
arslan-cyberTITU <rasulov.titu@gmail.com>
2022-04-04 16:38:14 +0500

    commit

--- gunicorn.py
+++ gunicorn.py
@@ -6,28 +6,18 @@ load_dotenv()
 
 bind = "0.0.0.0:3000"
 
-# worker_class = "uvicorn.workers.UvicornWorker"
-# workers = multiprocessing.cpu_count () * 2 + 1
-# debug = os.environ.get("debug", "false") == "true"
-# max_requests = 999999
-# reload = debug
-# backlog = 2048
-# worker_connections = 1024
-# timeout = 30
-# keepalive = 2
-# limit_request_fields = 32768
-# limit_request_line = 8190
-# errorlog = '-'
-# accesslog = '-'
-
-worker_class = os.environ['WORKER_CLASS']
-workers = os.environ['NUM_WORKERS']
-backlog = os.environ['BACKLOG']
-timeout = os.environ['TIMEOUT']
-accesslog = '-'
-errorlog = '-'
+worker_class = "async"
+workers = multiprocessing.cpu_count () * 2 + 1
+debug = os.environ.get("debug", "false") == "true"
 max_requests = 999999
+reload = debug
+backlog = 2048
 worker_connections = 1024
+timeout = 30
 keepalive = 2
+limit_request_fields = 32768
+limit_request_line = 8190
+errorlog = '-'
+accesslog = '-'
 
 
