676753d45f5b37a84df00512f536f7c5d73783d6
arslan-cyberTITU <rasulov.titu@gmail.com>
2022-04-08 17:40:24 +0500

    commit

--- crud.py
+++ crud.py
@@ -267,7 +267,7 @@ async def read_profile(db: Session, req: GetProfile):
             res["phone_numbers"] = get_phone_number
         new_list.append(res)
     result = new_list
-    # result = sorted(result, key=lambda d: d["is_VIP"], reverse=True)
+    result = sorted(result, key=lambda d: d["is_VIP"], reverse=True)
     if result:
         return result
     else:
